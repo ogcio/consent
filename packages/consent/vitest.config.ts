122d5832e2fdb80346962ff61ae7f4bec2a50642
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-08-20 16:36:28 +0200

    chore(): fix CI

--- packages/consent/vitest.config.ts
+++ packages/consent/vitest.config.ts
@@ -7,9 +7,13 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
     css: true,
+    reporters: process.env.CI ? ["default", "junit"] : ["default"],
+    outputFile: {
+      junit: "./results.xml",
+    },
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "html", "cobertura"],
       exclude: [
         "node_modules/",
         "src/test/",
